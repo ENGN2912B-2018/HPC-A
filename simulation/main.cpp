c3404a8503dc0f2091b1273756e72fadf9482095
Kit Sum Wu <kitsum_wu@brown.edu>
2018-12-01 22:05:35 -0500

    Create main.cpp

---
+++ simulation/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <ios>
+#include <stdlib.h>
+#include <exception>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <regex>
+#include "RBConVSim.h"
+
+using namespace std;
+
+// main function
+int main() {
+
+RBConvSim()
+
+	return 0;
+}
