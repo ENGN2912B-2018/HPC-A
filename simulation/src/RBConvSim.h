c481950e646fa11c65abef5032248029ffd8f913
Kit Sum Wu <guest145@node407.oscar.ccv.brown.edu>
2018-12-05 20:02:08 -0500

    changed simulation parameters, added velocity magnitude output

--- simulation/src/RBConvSim.h
+++ simulation/src/RBConvSim.h
@@ -3,9 +3,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <cmath>
-#include <thread>
 
 using namespace std;
 
@@ -110,29 +108,26 @@ class RBConvSim {
     void runSimulation()
         {
 	    system("cd $FOAM_RUN/tutorials/heatTransfer/buoyantBoussinesqPimpleFoam/RBConvection && buoyantBoussinesqPimpleFoam");
+	    system("cd $FOAM_RUN/tutorials/heatTransfer/buoyantBoussinesqPimpleFoam/RBConvection && postProcess -func \"mag(U)\"");
+
 	    simRun_ = true;
-        }
-	
-    vector<vector<double> > getTfield();
-    vector<vector<vector<double> > > getUfield();
+        }	
 
   private:
     //DO NOT CHANGE
     double L_ = 1; //simulation box width
-    double h_ = .4; //simulation box height
+    double h_ = .50; //simulation box height
     double TCeil_ = 280; // temperature at y = h;
-    double horzmesh_ = 40;
-    double vertmesh_ = 40;
-    double meshsize_ = 1600;
+    double horzmesh_ = 100;
+    double vertmesh_ = 50;
+    double meshsize_ = 5000;
     double tend_ = 2000; //simulation end time
-    double deltat_ = 100; // simulation time step
+    double deltat_ = 20; // simulation time step
     double g = 9.81; // acceleration of gravity
 
     //CHANGED BY PROGRAM
     double Ra_; //Rayleigh Number
     double deltaT_; //temperature difference
-    vector<vector<double> > Tfield_; //scalar x space x time
-    vector<vector<vector<double> > > Ufield_; //3D vector x space x time
     bool simRun_ = false;
 
     //CHANGED DIRECTLY BY USER
@@ -146,37 +141,7 @@ class RBConvSim {
 	    Ra_ = Pr_*beta_*g*deltaT_*pow(h_,3)/pow(nu_,2);
 	    deltaT_ = TFloor_ - TCeil_;
 	    system("cd $FOAM_RUN/tutorials/heatTransfer/buoyantBoussinesqPimpleFoam/RBConvection && foamListTimes -rm");
-	    Tfield_.clear();
-	    Ufield_.clear();
 	    simRun_ = false;
 	}
 };
-/*
-    vector<vector<double> > RBConvSim::getTfield()
-        {
-	if (!simRun_)
-	    {
-		//some kind of exception
-	    }
-	if Tfield_.empty()
-	    {
-	        //get data from file
-	    }
-	return Tfield;
-        }
 
-    vector<vector<vector<double> > > RBConvSim::getUfield()
-        {
- 	
-        if (!simRun_)
-	    {
-		//some kind of exception
-	    }
-	if Ufield_.empty()
-	    {
-	        //get data from file
-	    }
-	return Ufield;
-        }
-        }
-*/
